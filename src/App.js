56f89555d467bb24687690df1c778cc31be98c5e
ThienCao <caothien029@gmail.com>
2022-06-21 20:54:51 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
+import React, { Suspense } from 'react'
 import Applayout from './/AppLayout.js';
 import './App.css';
-
+import Loading from './Loading';
+const LazyApp = React.lazy(() => import('./AppLayout.js'));
 function App() {
   return (
     <div className="App">
-      <Applayout />
+      <Suspense fallback={<Loading />}>
+        <LazyApp />
+      </Suspense>
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,4 @@
+import './loading.css'
+export default () => {
+    return <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+}
